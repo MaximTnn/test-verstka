fa705f90ee9e6ac36e31e066ebd55ea7d594c75f
Max <tyumenbaev.m@gmail.com>
2021-04-05 10:26:53 +0300

    feat: add modal window, check validation, fix bag

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
-let menuBtn = document.querySelector('.menu__btn')
-let menu = document.querySelector('.menu')
-let header = document.querySelector('.header')
+const menuBtn = document.querySelector('.menu__btn')
+const menu = document.querySelector('.menu')
+const header = document.querySelector('.header')
+const buttonCall = document.querySelector('.intro__btn')
+const modalWindow = document.querySelector('.modal-window')
+const inputs = document.querySelectorAll('input')
+const checkAgreement = document.querySelector('.form__input-agreement')
+
+modalWindow.addEventListener('click',modalWindowOpen())
+
+
+
+function modalWindowOpen(a) {
+    const modalWindow = document.querySelector('.modal-window')
+    modalWindow.classList.toggle('active')
+    console.log(modalWindow.classList.contains('active'))
+    if (!modalWindow.classList.contains('active')) {
+        header.classList.add('active')
+        header.style.boxShadow = "0 5px 5px #00000021"
+    } else {
+        header.classList.remove('active')
+        header.style.boxShadow = "none"
+    }
+
+}
 
 menuBtn.addEventListener('click', function() {
     let subMenuActive = document.querySelectorAll('.sub-menu')
@@ -20,7 +42,9 @@ let isMobile = {
 	Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
 	any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
 };
-		let body=document.querySelector('body');
+
+let body=document.querySelector('body');
+
 if(isMobile.any()){
 		body.classList.add('touch');
 		let arrow=document.querySelectorAll('.menu-list__arrow-wraper');
@@ -49,11 +73,56 @@ window.addEventListener('scroll', function(){
     let scroll = window.scrollY
     let opacity
     console.log(scroll, opacity)
+   
     if (scroll <= maxScroll) {
         opacity = 1 - (scroll / maxScroll)
     }
     if (scroll > 200) {
-        header.style.backgroundColor = '#ffffff'
+        header.classList.add('active')
+    } else {
+        header.classList.remove('active')
     }
     videoBg.style.opacity = opacity
+    if (menu.classList.contains('active') || !modalWindow.classList.contains('active')) {
+        header.classList.add('active')
+    }
 });
+
+
+
+
+checkAgreement.addEventListener('click', checkSubmit())
+inputs.forEach(el => el.addEventListener("keyup", event => validation(event)))
+
+function validation(event) {
+    console.log("hi")
+    if (!event.target.validity.valid) {
+        event.target.parentElement.querySelector('.form__invalid').classList.add('active')
+        event.target.parentElement.querySelector('.form__valid').classList.remove('active')
+    } else {
+        event.target.parentElement.querySelector('.form__invalid').classList.remove('active')
+        event.target.parentElement.querySelector('.form__valid').classList.add('active')
+    }
+    checkSubmit()
+}
+
+function checkSubmit() {
+    console.log('hi 2')
+    const name = document.querySelector('.form__input-name')
+    const phone = document.querySelector('.form__input-phone')
+    const email = document.querySelector('.form__input-email')
+    const agreement = document.querySelector('.form__input-agreement')
+    const submitBtn = document.querySelector('.form__submit')
+
+    if (name.validity.valid &
+        phone.validity.valid &
+        email.validity.valid &
+        agreement.validity.valid) 
+        {
+            submitBtn.removeAttribute("disabled")
+        }
+        else
+        {
+            submitBtn.setAttribute("disabled", "disabled")
+        }
+}   
